f4dccc0ac9ac221718d65b1b406039356fbb8d48
basarekinci <basarekinci02@outlook.com>
2026-08-29 17:28:17 +0300

    feat(match3): wire gameplay services in scene installer

--- Assets/Match3/Scripts/Installers/Match3SceneInstaller.cs
+++ Assets/Match3/Scripts/Installers/Match3SceneInstaller.cs
@@ -1,6 +1,12 @@
 using System;
+using Match3.Controller;
 using Match3.Data;
+using Match3.Model.Generation;
+using Match3.Model.Gravity;
+using Match3.Model.Matching;
 using Match3.Model.Settings;
+using Match3.Model.Special;
+using Match3.View;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -16,6 +22,26 @@ namespace Match3.Installers
         {
             builder.RegisterInstance(Load<BoardSettings>(BoardSettingsPath)).As<IBoardSettings>();
             builder.RegisterInstance(Load<ScoreSettings>(ScoreSettingsPath)).As<IScoreSettings>();
+            builder.Register<MatchFinder>(Lifetime.Singleton).As<IMatchFinder>();
+            builder.Register<MoveScanner>(Lifetime.Singleton).As<IMoveScanner>();
+            builder.RegisterInstance(new System.Random());
+            builder.Register<BoardGenerator>(Lifetime.Singleton).As<IBoardGenerator>();
+            builder.Register<GravityResolver>(Lifetime.Singleton).As<IGravityResolver>();
+            builder.Register<SpecialTileEffects>(Lifetime.Singleton);
+            builder.Register<ChainResolver>(Lifetime.Singleton);
+            builder.Register<SpecialCombinationResolver>(Lifetime.Singleton);
+            builder.Register<BoardGeometry>(Lifetime.Singleton);
+            builder.Register<TilePool>(Lifetime.Singleton);
+            builder.Register<BoardView>(Lifetime.Singleton);
+            builder.Register<BoardFeedbackView>(Lifetime.Singleton);
+            builder.Register<BoardController>(Lifetime.Singleton);
+            builder.Register<InputController>(Lifetime.Singleton);
+            builder.RegisterComponentOnNewGameObject<BoardInputView>(Lifetime.Singleton);
+            builder.RegisterBuildCallback(container => container.Resolve<BoardView>());
+            builder.RegisterBuildCallback(container => container.Resolve<BoardFeedbackView>());
+            builder.RegisterBuildCallback(container => container.Resolve<BoardController>());
+            builder.RegisterBuildCallback(container => container.Resolve<InputController>());
+            builder.RegisterBuildCallback(container => container.Resolve<BoardInputView>());
         }
 
         private static TSettings Load<TSettings>(string path) where TSettings : ScriptableObject
